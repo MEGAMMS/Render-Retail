cfe1519b74814faea546f9dae97bc333b05dfaec
Mouaaz Sweid <mmsweid@gmail.com>
2024-01-11 14:08:21 +0300

    made core.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,4 @@
-
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include <iostream>
+#include "core.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
