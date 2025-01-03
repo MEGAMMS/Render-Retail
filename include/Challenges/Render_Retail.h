00e09433ec5be4df0b028aa46448ad58de2740c1
Ken <ahmadsoud63@gmail.com>
2025-01-03 15:06:40 +0300

    add basic functions

--- include/Challenges/Render_Retail.h
+++ include/Challenges/Render_Retail.h
@@ -4,3 +4,14 @@
 #include "Render/ShaderProgram.h"
 #include "Render/VertexArray.h"
 #include "Application/Scene.h"
+
+class Render_Retail
+{
+    public:
+        Render_Retail();
+        void update(float dt);
+        void render() ;
+        void onKeyEvent(int32_t key, int32_t scancode, int32_t action, int32_t mode);
+    private:
+
+};
