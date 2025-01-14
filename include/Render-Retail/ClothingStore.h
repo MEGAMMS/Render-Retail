5b63832941f5df82c6261b33024d02a48e2525fe
Mouaaz Sweid <mmsweid@gmail.com>
2025-01-14 14:26:49 +0300

    add texture scale ablitiy

--- include/Render-Retail/ClothingStore.h
+++ include/Render-Retail/ClothingStore.h
@@ -2,21 +2,19 @@
 
 #include "Objects/Box.h"
 #include "Objects/Object.h"
-#include "core.h"
-#include "Render-Retail/Stores/StoreWalls.h"
 #include "Render-Retail/Objects/Table.h"
+#include "Render-Retail/Stores/StoreWalls.h"
+#include "core.h"
 
-class ClothingStore : public Object
-{
-    public:
-        ClothingStore();
-        void update(float dt) override;
-        void render(glm::mat4& mvp, glm::vec3 lightPos, glm::vec3 lightColor, glm::vec3 viewPos);
-        void onKeyEvent(int32_t key, int32_t scancode, int32_t action, int32_t mode) override;
-        void onCursorPositionEvent(double x, double y) override;
-    private:
-        std::shared_ptr<StoreWalls> m_room;
-        std::shared_ptr<Table> m_desk;
-        
+class ClothingStore : public Object {
+   public:
+    ClothingStore();
+    void update(float dt) override;
+    void render(glm::mat4& mvp, glm::vec3 lightPos, glm::vec3 lightColor, glm::vec3 viewPos);
+    void onKeyEvent(int32_t key, int32_t scancode, int32_t action, int32_t mode) override;
+    void onCursorPositionEvent(double x, double y) override;
 
+   private:
+    std::shared_ptr<StoreWalls> m_room;
+    std::shared_ptr<Table> m_desk;
 };
