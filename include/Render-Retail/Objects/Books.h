3d9b7ac2d1733b216c938de65813499059513989
Ken <ahmadsoud63@gmail.com>
2025-01-16 15:23:19 +0300

    finalize book store

--- include/Render-Retail/Objects/Books.h
+++ include/Render-Retail/Objects/Books.h
@@ -3,26 +3,20 @@
 #include "Objects/Box.h"
 #include "Objects/Object.h"
 #include "Render-Retail/Objects/Lamp.h"
-#include "Render-Retail/Objects/Books.h"
 #include "Render-Retail/Objects/Shelves.h"
 #include "Render-Retail/Objects/Table.h"
 #include "Render-Retail/Objects/Wardrobe.h"
 #include "Render-Retail/Stores/StoreWalls.h"
 #include "core.h"
 
-class BookStore : public Object {
+class Books : public Object {
    public:
-    BookStore();
+    Books(unsigned int books);
     void update(float dt) override;
     void render(glm::mat4& mvp, glm::vec3 lightPos, glm::vec3 lightColor, glm::vec3 viewPos);
     void onKeyEvent(int32_t key, int32_t scancode, int32_t action, int32_t mode) override;
     void onCursorPositionEvent(double x, double y) override;
 
    private:
-    std::shared_ptr<StoreWalls> m_room;
-    std::shared_ptr<Table> m_desk;
-    std::vector<std::shared_ptr<Shelves>> m_shelves;
-    std::vector<std::shared_ptr<Lamp>> m_lamps;
-    std::vector<std::shared_ptr<Wardrobe>> m_wardrobes;
-    std::vector<std::shared_ptr<Books>> m_books;
+    std::vector<std::shared_ptr<Box>> m_books;
 };
